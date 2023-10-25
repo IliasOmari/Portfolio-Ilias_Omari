24d9be3e806bbfba2f6b8eb8d2497d8208f9d3a1
IliasOmari <ilias.omari@student.ehb.be>
2023-10-25 10:38:36 +0200

    added image of me and adapted some CSS

--- Portfolio_Ilias_Omari/src/components/pages/Home.jsx
+++ Portfolio_Ilias_Omari/src/components/pages/Home.jsx
@@ -2,19 +2,26 @@ import Navbar from "../Navbar";
 import buttons from "../../assets/buttons.png";
 import scroll from "../../assets/scroll.png";
 import "./home-modules.css";
+import Me from "../../assets/ilias-remove.png";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="ilias-image">
-        <div className="buttons">
-          <img src={buttons} alt="Buttons" />
+      <div className="presentation">
+        <div className="ilias-image">
+          <div className="buttons">
+            <img src={buttons} alt="Buttons" />
+          </div>
+          <div className="ilias">
+            <div className="image">
+              <img src={Me} />
+            </div>
+          </div>
         </div>
-        <div className="ilias"></div>
-      </div>
-      <div className="ilias-name">
-        <h1>Ilias Omari</h1>
+        <div className="ilias-name">
+          <h1>Ilias Omari</h1>
+        </div>
       </div>
 
       <button className="scroll">
