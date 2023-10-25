41af0be487ac21752732e80666893d8de7cda5e8
IliasOmari <ilias.omari@student.ehb.be>
2023-10-25 14:17:34 +0200

    finished homepage mobile+tablet

--- Portfolio_Ilias_Omari/src/components/pages/Home.jsx
+++ Portfolio_Ilias_Omari/src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../Navbar";
 import buttons from "../../assets/buttons.png";
-import scroll from "../../assets/scroll.png";
+// import scroll from "../../assets/scroll.png";
 import "./home-modules.css";
 import Me from "../../assets/ilias-remove.png";
 
@@ -22,11 +22,36 @@ const Home = () => {
         <div className="ilias-name">
           <h1>Ilias Omari</h1>
         </div>
+        {/* <button className="scroll">
+          <img className="scroll-img" src={scroll} alt="scroll button" />
+        </button> */}
       </div>
 
-      <button className="scroll">
-        <img className="scroll-img" src={scroll} alt="scroll button" />
-      </button>
+      <div className="ilias-about">
+        <div className="buttons-cv">
+          <img src={buttons} alt="Buttons" />
+        </div>
+        <div className="context">
+          <div className="about-text">
+            <br /> <br />
+            <p>
+              Welcome to my portfolio! I'm Ilias, a Multimedia and Creative
+              Technologies student, specializing in UX/UI design and
+              development.
+              <br /> <br />
+              Inside my portfolio, I invite you to explore my passion for
+              crafting seamless, visually stunning digital experiences.
+              <br /> <br /> Let's collaborate and bring your digital dreams to
+              life.
+            </p>
+          </div>
+          <button className="download">Download my CV</button>
+        </div>
+      </div>
+
+      <div className="welcome-text">
+        <h1>Hi 👋🏻, I am </h1>
+      </div>
     </>
   );
 };
