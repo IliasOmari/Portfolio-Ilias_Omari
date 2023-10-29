3996eb4f15c0e19c9947bdc6ef015754c699e44a
IliasOmari <ilias.omari@student.ehb.be>
2023-10-29 22:30:30 +0100

    created contact form

--- Portfolio_Ilias_Omari/src/components/pages/Contact.jsx
+++ Portfolio_Ilias_Omari/src/components/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../Navbar";
 import "./skills-modules.css";
+import "./contact-modules.css";
 
 const Contact = () => {
   return (
@@ -7,6 +8,29 @@ const Contact = () => {
       <Navbar />
       <div className="title">
         <h1>Contact</h1>
+      </div>
+
+      <div className="form">
+        <div className="form_area">
+          <form action="">
+            <div className="form_group">
+              <label className="sub_title">Name</label>
+              <input type="text" className="form_style" id="name" />
+            </div>
+
+            <div className="form_group">
+              <label className="sub_title">Email</label>
+              <input type="email" className="form_style" id="email" />
+            </div>
+            <div className="form_group">
+              <label className="sub_title">Message</label>
+              <input type="text" className="form_style-message" id="text" />
+            </div>
+            <div className="form-button">
+              <button className="btn">Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
