6b7244bdd12357939f329b4ae6b7fe9710248f31
IliasOmari <ilias.omari@student.ehb.be>
2023-10-30 19:35:48 +0100

    created first card of projects

--- Portfolio_Ilias_Omari/src/components/pages/Projects.jsx
+++ Portfolio_Ilias_Omari/src/components/pages/Projects.jsx
@@ -1,5 +1,7 @@
 import Navbar from "../Navbar";
 import "./skills-modules.css";
+import NftUniverse from "../../assets/nft-universe.png";
+import "./projects-modules.css";
 
 const Projects = () => {
   return (
@@ -7,6 +9,25 @@ const Projects = () => {
       <Navbar />
       <div className="title">
         <h1>Projects</h1>
+      </div>
+
+      <div className="project-cards">
+        <div className="project-1">
+          <div className="project-1-img">
+            <img src={NftUniverse} alt="project of Nft Universe" />
+          </div>
+          <div className="project-1-title">
+            <h2>NFT Universe</h2>
+          </div>
+          <div className="project-categories">
+            <div className="categorie-design">
+              <p>Design</p>
+            </div>
+            <div className="categorie-dev">
+              <p>Dev</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
