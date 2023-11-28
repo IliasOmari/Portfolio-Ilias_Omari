5b176370f12924c35b0078bd8dd8d3245dc1d9e9
IliasOmari <ilias.omari@student.ehb.be>
2023-11-28 11:53:01 +0100

    added expert projects into the project page

--- Portfolio_Ilias_Omari/src/components/pages/Projects.jsx
+++ Portfolio_Ilias_Omari/src/components/pages/Projects.jsx
@@ -8,13 +8,17 @@ import JEF from "../../assets/JEF.jpg";
 import PopArt from "../../assets/popart.png";
 import Motion from "../../assets/motion.png";
 import Kasteel from "../../assets/kasteel.png";
+import Quiz from "../../assets/quiz.png";
+import Animations from "../../assets/animation.png";
+import ThreeD from "../../assets/3d.png";
+import Login from "../../assets/login.png";
 
 const projects = [
   {
     id: "NFT",
     title: "NFT Universe",
     img: NftUniverse,
-    subtitle: "personal project",
+    subtitle: "School project",
     categorie: "Design & Development",
     link: "https://ehb-mct.github.io/web-2-frontend-22-23-IliasOmari/dist/index.html",
   },
@@ -50,7 +54,7 @@ const projects = [
     id: "MONEY",
     title: "The history of money",
     img: Motion,
-    subtitle: "personal project",
+    subtitle: "School project",
     categorie: "Motion graphics",
     link: "/projects/history-of-money",
   },
@@ -59,9 +63,45 @@ const projects = [
     id: "CASTLE",
     title: "Castle of Gaasbeek",
     img: Kasteel,
-    subtitle: "personal project",
+    subtitle: "School project",
     categorie: "Drones & Motion ",
     link: "/projects/castel-of-gaasbeek",
+  },
+
+  {
+    id: "PROTOTYPE1",
+    title: "Quiz App",
+    img: Quiz,
+    subtitle: "Expert Lab",
+    categorie: "Front-end",
+    link: "https://pop-art.onrender.com/project/pop-art",
+  },
+
+  {
+    id: "PROTOTYPE2",
+    title: "Animations",
+    img: Animations,
+    subtitle: "Expert Lab",
+    categorie: "Front-end",
+    link: "/projects/animations",
+  },
+
+  {
+    id: "PROTOTYPE3",
+    title: "3D Models",
+    img: ThreeD,
+    subtitle: "Expert lab",
+    categorie: "Front-end ",
+    link: "https://pop-art.onrender.com/project/pop-art",
+  },
+
+  {
+    id: "PROTOTYPE4",
+    title: "Login System",
+    img: Login,
+    subtitle: "Expert Lab",
+    categorie: "Front-end & Back-end ",
+    link: "https://pop-art.onrender.com/project/pop-art",
   },
 ];
 
